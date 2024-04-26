90b75cbba575fc7177f505804ece289658a373e9
Bumba-Caetano <bumbacaetano95@gmail.com>
2024-04-26 21:51:13 +0100

    d

--- js/music.js
+++ js/music.js
@@ -449,3 +449,69 @@ artistas_populares_recuar .addEventListener('click', ()=>{
     artistas_bx.scrollLeft  -=330
 })
   /**Fim das setas */
+  /**Fim das setas */
+/**Funçao de random, next e repetição */
+let shuffle = document.getElementsByClassName('shuffle')[0];
+shuffle.addEventListener('click', () => {
+    let a= shuffle.innerHTML;
+
+    switch (a) {
+          case "next":
+              shuffle.classList.add('bi-arrow-repeat');
+              shuffle.classList.remove('bi-music-note-beamed');
+              shuffle.classList.remove('bi-shuffle');
+              shuffle.innerHTML = 'repeat';
+              break;
+          case "repeat":
+              shuffle.classList.remove('bi-arrow-repeat');
+              shuffle.classList.remove('bi-music-note-beamed');
+              shuffle.classList.add('bi-shuffle');
+              shuffle.innerHTML = 'random';
+              break;
+          case "random":
+              shuffle.classList.remove('bi-arrow-repeat');
+              shuffle.classList.add('bi-music-note-beamed');
+              shuffle.classList.remove('bi-shuffle');
+              shuffle.innerHTML = 'next';
+              break;
+    }
+});
+/**Fim da Funçao de random, next e repetição */
+musica.addEventListener('ended', ()=>{
+        index ++
+    
+        musica.src= `audio/${index}.mp3`//Colocar(poster) e procurar musicas//
+        poster_melhores_musicas.src=`Imagens/telefones/${index}.jpg`      
+        musica.play() 
+        melhoresmusicas.classList.remove('bi-play-fill')
+        melhoresmusicas.classList.add('bi-pause-fill')
+        download_musica.href=`audio/${index}.mp3`//Baixar Musicas//
+        let TitulosMusicas = musicas.filter((els)=>{
+            return els.id==index
+        })
+        TitulosMusicas.forEach(elss =>{
+            let {nomeMusica}=elss
+            titulo.innerHTML= nomeMusica
+           // poster_melhores_musicas.src= poster
+           download_musica.setAttribute('download', nomeMusica);
+
+        })
+
+        makeAllBackground()
+        Array.from(document.getElementsByClassName('itens_sons'))[index - 1 ].style.background='rgb(105,105,105, .1)'
+        makeAllplays()
+        e1.target.classList.remove('bi-play-circle-fill')
+        e1.target.classList.add('bi-pause-circle-fill')
+        wave.classList.add('active1')
+        
+
+})
+const prox_musica = ()=>{
+     if (index == musicas.length) {
+        index=1
+        
+    } else {
+        index++
+        
+    }}
+
